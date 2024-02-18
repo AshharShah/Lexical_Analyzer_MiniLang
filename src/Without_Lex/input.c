b238fb723a704d8d3e572e5dfee9166446ca001d
AshharShah <ashhar.shah786@gmail.com>
2024-02-18 15:58:53 +0500

    Updated TestCase

--- src/Without_Lex/input.c
+++ src/Without_Lex/input.c
@@ -1,5 +1,23 @@
+int x = 3;
+if(x == 3){
+    print("Hello World");
+}
+
+x = x + 3;
+y = 10;
+x = y - x * x;
+print(x);
+print(y);
+
+char y = 'c';
+if(y == 'c'){
+    print("Y is C");
+}
+
 bool z = false;
 if(z == true){
     print("z is true");
 }
-int y = @hi;
+else{
+    print("z is false");
+}
